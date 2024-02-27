f8c381815fdfad50df8287b1772d59d894a6bf4a
João Marcos <joaomarcosm.silva@outlook.com>
2024-02-26 23:01:19 -0300

    create route to register new pets

--- src/app/pet/layout.tsx
+++ src/app/pet/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
-import "../../globals.css";
+import "../globals.css";
 import { TanstackQuery } from "@/providers/queryClientProvider";
 import { SideBar } from "@/components/SideBar";
 
---
+++ src/app/pet/register/page.tsx
@@ -0,0 +1,3 @@
+export default function RegisterPet() {
+  return <div>Registrar pet</div>;
+}
